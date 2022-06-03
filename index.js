70979e7d967f03d44482d851b88b0e7d43223161
kedricf <102987861+kedricf@users.noreply.github.com>
2022-06-03 13:05:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-let newDate =function (firstDate,secondDate){
-if(secondDate - firstDate)
-    console.log(newDate)
-}
+////let newDate =function (firstDate,secondDate){
+///if(secondDate - firstDate)
+    //return(newDate)
+//}
     
+  function getDays(March14, March15) {
+    if (March15 - March14)
+        ;
+    return ('1 day');
 
-  
+}
